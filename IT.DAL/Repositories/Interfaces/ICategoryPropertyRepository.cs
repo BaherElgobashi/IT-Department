3914c9d459a6ff20f1a86963d38c28bec7004437
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-07-08 21:30:59 +0300

    Add ICategoryPropertyRepository interface.

---
+++ IT.DAL/Repositories/Interfaces/ICategoryPropertyRepository.cs
@@ -0,0 +1,17 @@
+﻿using IT.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT.DAL.Repositories.Interfaces
+{
+    public interface ICategoryPropertyRepository
+    {
+        Task<List<CategoryProperty>> GetAllAsync();
+        Task<List<PropertyItem>> GetPropertiesByCategoryIdAsync(int categoryId);
+        Task AddAsync(CategoryProperty categoryProperty);
+        Task DeleteAsync(int categoryId, int propertyItemId);
+    }
+}
