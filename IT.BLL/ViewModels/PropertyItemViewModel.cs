72e1ab1a94f5bab63118d6246f0b0677c0cb26a6
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-07-08 21:51:24 +0300

    Add PropertyItemViewModel to the ViewModels Folder.

---
+++ IT.BLL/ViewModels/PropertyItemViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT.BLL.ViewModels
+{
+    public class PropertyItemViewModel
+    {
+        [Required]
+        public string Description { get; set; }
+    }
+}
