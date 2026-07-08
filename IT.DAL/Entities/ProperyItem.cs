775ce69111922f9962541f2c73da32b4eaa0af48
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-07-08 15:53:32 +0300

    Add PropertyItem Class to Entities Folder and add Properties inside it.

---
+++ IT.DAL/Entities/ProperyItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT.DAL.Entities
+{
+    public class ProperyItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        
+    }
+}
