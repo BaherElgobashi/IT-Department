17f6ed83c26e12f5c133caab15aaaea76e5da543
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-07-08 21:50:13 +0300

    Add DeviceViewModel to the ViewModels Folder.

---
+++ IT.BLL/ViewModels/DeviceViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT.BLL.ViewModels
+{
+    public class DeviceViewModel
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime AcquisitionDate { get; set; }
+
+        public string Memo { get; set; }
+
+        [Required]
+        public int DeviceCategoryId { get; set; }
+
+        // Update.
+        public Dictionary<int, string> PropertyValues { get; set; } = new();
+
+        // Read.
+        public Dictionary<int, string> DisplayPropertyValues { get; set; } = new();
+    }
+}
