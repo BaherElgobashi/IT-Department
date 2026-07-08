55794c46b1d1b2b5d21ff8a593e55002ede2b507
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-07-08 20:37:58 +0300

    Add IDeviceCategoryRepository to the Interface Folder.

---
+++ IT.DAL/Repositories/Interfaces/IDeviceCategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using IT.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT.DAL.Repositories.Interfaces
+{
+    public interface IDeviceCategoryRepository
+    {
+        Task<IEnumerable<DeviceCategory>> GetDeviceCategoriesAsync();
+
+        Task<DeviceCategory> GetDeviceCategotyById(int id);
+        Task Add(DeviceCategory deviceCategory);
+        void Update(DeviceCategory deviceCategory);
+        Task Delete(DeviceCategory deviceCategory);
+    }
+}
