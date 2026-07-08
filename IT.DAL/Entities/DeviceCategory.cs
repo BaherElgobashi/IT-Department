50f36cfab05986d15ff1b93123fec6840726ee80
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-07-08 15:45:55 +0300

    Add DeviceCategory class and add Properties inside it.

---
+++ IT.DAL/Entities/DeviceCategory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT.DAL.Entities
+{
+    public class DeviceCategory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+
+    }
+}
