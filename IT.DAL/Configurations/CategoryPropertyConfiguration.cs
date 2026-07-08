9575051b0f1b1a645f03f5de69b0a367fe9a4648
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-07-08 16:26:37 +0300

    Add CategoryPropertyConfiguration inside the Configuration Folder to make a composite key for CategoryProperty class.

---
+++ IT.DAL/Configurations/CategoryPropertyConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using IT.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT.DAL.Configurations
+{
+    internal class CategoryPropertyConfiguration : IEntityTypeConfiguration<CategoryProperty>
+    {
+        public void Configure(EntityTypeBuilder<CategoryProperty> builder)
+        {
+            builder.HasKey(cp => new { cp.DeviceCategoryId, cp.PropertyItemId });
+        }
+    }
+}
