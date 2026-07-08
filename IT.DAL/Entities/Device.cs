c09175f46d8833b03542a0a30edae37fc81e3ed6
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-07-08 15:43:16 +0300

    Add Properties inside the Decive Class.

--- IT.DAL/Entities/Device.cs
+++ IT.DAL/Entities/Device.cs
@@ -8,5 +8,11 @@ namespace IT.DAL.Entities
 {
     public class Device
     {
+        public string Id { get; set; } = default!;
+        public string DeviceName { get; set; } = default!; // Show the Name of the code only not the relation.
+        public string DeviceCategory { get; set; } = default!;
+        public DateTime AcquisitionDate { get; set; }
+        public string? Memo { get; set; }
+
     }
 }
