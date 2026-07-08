d6f41a3b7cca0c8860a1a8ca3ce83e4be061cec8
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-07-08 16:57:04 +0300

    Add IDeviceRepository and Add all Signatures inside it.

---
+++ IT.DAL/Repositories/Interfaces/IDeviceRepository.cs
@@ -0,0 +1,19 @@
+﻿using IT.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT.DAL.Repositories.Interfaces
+{
+    public interface IDeviceRepository
+    {
+        Task<IEnumerable<Device>> GetDevicesAsync();
+        Task<Device?> GetDeviceById(string id);
+
+        Task addDevice(Device device);
+        void updateDevice(Device device);
+        Task deleteDevice(string id);
+    }
+}
