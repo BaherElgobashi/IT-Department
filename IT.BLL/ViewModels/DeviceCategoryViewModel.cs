c8c21a16b854d1065a70ddbc21d8aecb95edd2a6
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-07-08 21:52:45 +0300

    Add DeviceCategoryViewModel to the ViewModels Folder.

---
+++ IT.BLL/ViewModels/DeviceCategoryViewModel.cs
@@ -0,0 +1,21 @@
+﻿using IT.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT.BLL.ViewModels
+{
+    public class DeviceCategoryViewModel
+    {
+        [Required(ErrorMessage = "Category name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "You must select at least one property")]
+        public List<int> SelectedPropertyIds { get; set; } = new();
+
+        public List<PropertyItem> AllProperties { get; set; } = new();
+    }
+}
