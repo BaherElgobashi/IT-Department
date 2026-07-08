4827c6fd297c5760390bdecec1a4670bf650f63c
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-07-08 21:07:16 +0300

    Add IDevicePropertyValueRepository interface.

---
+++ IT.DAL/Repositories/Interfaces/IDevicePropertyValueRepository.cs
@@ -0,0 +1,16 @@
+﻿using IT.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT.DAL.Repositories.Interfaces
+{
+    public interface IDevicePropertyValueRepository
+    {
+        Task<List<DevicePropertyValue>> GetByDeviceIdAsync(string deviceId);
+        Task AddRangeAsync(List<DevicePropertyValue> values);
+        Task DeleteByDeviceIdAsync(string deviceId);
+    }
+}
