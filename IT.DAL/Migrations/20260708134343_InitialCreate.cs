d20df752f148fc7f33410143ef78587f71c3ca3c
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-07-08 16:44:49 +0300

    Download the Design Packahe inside the IT.Pl Project.

---
+++ IT.DAL/Migrations/20260708134343_InitialCreate.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IT.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DeviceCategories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DeviceCategories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PropertyItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PropertyItems", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Devices",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    DeviceName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AcquisitionDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Memo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DeviceCategoryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Devices", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Devices_DeviceCategories_DeviceCategoryId",
+                        column: x => x.DeviceCategoryId,
+                        principalTable: "DeviceCategories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CategoryProperties",
+                columns: table => new
+                {
+                    DeviceCategoryId = table.Column<int>(type: "int", nullable: false),
+                    PropertyItemId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CategoryProperties", x => new { x.DeviceCategoryId, x.PropertyItemId });
+                    table.ForeignKey(
+                        name: "FK_CategoryProperties_DeviceCategories_DeviceCategoryId",
+                        column: x => x.DeviceCategoryId,
+                        principalTable: "DeviceCategories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CategoryProperties_PropertyItems_PropertyItemId",
+                        column: x => x.PropertyItemId,
+                        principalTable: "PropertyItems",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DevicePropertyValues",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Value = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DeviceId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    PropertyItemId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DevicePropertyValues", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DevicePropertyValues_Devices_DeviceId",
+                        column: x => x.DeviceId,
+                        principalTable: "Devices",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DevicePropertyValues_PropertyItems_PropertyItemId",
+                        column: x => x.PropertyItemId,
+                        principalTable: "PropertyItems",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CategoryProperties_PropertyItemId",
+                table: "CategoryProperties",
+                column: "PropertyItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DevicePropertyValues_DeviceId",
+                table: "DevicePropertyValues",
+                column: "DeviceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DevicePropertyValues_PropertyItemId",
+                table: "DevicePropertyValues",
+                column: "PropertyItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Devices_DeviceCategoryId",
+                table: "Devices",
+                column: "DeviceCategoryId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CategoryProperties");
+
+            migrationBuilder.DropTable(
+                name: "DevicePropertyValues");
+
+            migrationBuilder.DropTable(
+                name: "Devices");
+
+            migrationBuilder.DropTable(
+                name: "PropertyItems");
+
+            migrationBuilder.DropTable(
+                name: "DeviceCategories");
+        }
+    }
+}
