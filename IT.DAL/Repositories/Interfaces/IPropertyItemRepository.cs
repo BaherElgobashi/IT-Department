4dfc46433a45aa12941f15b35e6ddc72fc03b559
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-07-08 21:18:14 +0300

    Add IPropertyItemRepository Interface to the Interface Folder.

---
+++ IT.DAL/Repositories/Interfaces/IPropertyItemRepository.cs
@@ -0,0 +1,18 @@
+﻿using IT.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT.DAL.Repositories.Interfaces
+{
+    public interface IPropertyItemRepository
+    {
+        Task<IEnumerable<PropertyItem>> GetAllPropertyItemAsync();
+        Task<PropertyItem> GetPropertyItemAsync(int id);
+        Task Add(PropertyItem item);
+        Task Update (PropertyItem item);
+        Task Delete(int id);
+    }
+}
