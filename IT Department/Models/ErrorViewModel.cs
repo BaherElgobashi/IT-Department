ffba93a4095111e055ff1215c198101db546c25b
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-07-08 15:27:45 +0300

    Add project files.

---
+++ IT Department/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IT_Department.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
